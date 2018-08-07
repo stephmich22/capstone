e0d24c02526419fa22916d659c13dd0d7327e350
stephmich22 <semichalopoulos@email.neit.edu>
2018-08-07 09:53:39 -0400

    First draft

--- functions.php
+++ functions.php
@@ -5,6 +5,29 @@
 //TABLES & COLUMNS
 
 
+// CATEGORY SQL STATEMENTS ------------------------------------------------
+
+//add new category
+
+//update existing category name
+
+//delete category
+
+
+//FLASHCARD SQL FUNCTIONS -------------------------------------------------
+
+//add new flashcard
+
+//update existing flashcard
+
+//delete flashcard
+
+
+// AUTHENTICATION FUNCTIONS -----------------------------------------------
+
+
+//ACCOUNT FUNCTIONS & SQL STATEMENTS --------------------------------------
+
 
 
 
